cc0af4a39cf14e90586c1f4b9aede2e264bcf01a
Penny <penny@penny.moe>
2024-03-20 16:49:21 -0400

    Send LoginResponse and fill user properly

--- SVServer/EventListener.cs
+++ SVServer/EventListener.cs
@@ -22,16 +22,28 @@ public class EventListener : PacketHandler<SVConnection>
         {
             return;
         }
+
+        var loginResponse = new LoginResponse();
         
         if (Program.ConnectedUsers.Exists(c => c.Nick == login.Nick))
         {
+            loginResponse.Success = false;
+            conn.Send(loginResponse, MessageType.LoginResponse);
             Program.DisconnectUser(conn, "User already exists!");
             Console.WriteLine($"{login.Nick} from {conn.Address} tried to connect with taken nick!");
             return;
         }
 
+        conn.FillUserInfo(login.Nick);
         Program.UserAuthed(conn);
         Console.WriteLine($"Login approved from {conn.Address} with nick {login.Nick}");
+        loginResponse.Success = true;
+        conn.Send(loginResponse, MessageType.LoginResponse);
+
+        if (Program.ConnectedUsers.Count == 0)
+        {
+            conn.IsHost = true;
+        }
     }
 
     public override void OnSerializationException(MessagePackSerializationException exception, int packetID)
